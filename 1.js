dd5824e221f46d69f5ae4d0108f99de25f37fdb2
alex182 <format255@bk.ru>
2018-03-07 15:57:44 +0300

    11

---
+++ 1.js
@@ -0,0 +1,9 @@
+function almostIncreasingSequence(sequence) {
+    let newX = sequence.map((x,key) => {
+        x+1
+    });
+    console.log(newX);
+}
+
+var arr = [1,3,2,1];
+almostIncreasingSequence(arr);
