d1e1a2f0efc9b09abe7c45dbbc264ba8d3c75639
Овсянников Максим Викторович <m.ovsyannikov@informika.ru>
2018-03-13 21:20:32 +0300

    15 test done

--- js/renderCrosswordBirds.js
+++ js/renderCrosswordBirds.js
@@ -20,41 +20,103 @@ function renderCrosswordBirds($container){
         "                                        <div class=\"test-words__item tac\">\n" +
         "                                            <div class=\"test-words__cell\">\n" +
         "                                                <img src=\"img/bird1.png\" alt=\"\">\n" +
-        "                                                8 + 2 - 8 = <input class=\"test-words__input\" value=\"2\">\n" +
+        "                                                8 + 2 - 8 = <input data-number='2' class=\"test-words__input\" value=\"\">\n" +
         "                                            </div>\n" +
         "                                            <div class=\"test-words__cell\">\n" +
         "                                                <img src=\"img/bird2.png\" alt=\"\">\n" +
-        "                                                17 - 10 = <input class=\"test-words__input\" value=\"\">\n" +
+        "                                                17 - 10 = <input data-number='7' class=\"test-words__input\" value=\"\">\n" +
         "                                            </div>\n" +
         "                                            <div class=\"test-words__cell\">\n" +
         "                                                <img src=\"img/bird3.png\" alt=\"\">\n" +
-        "                                                16 - 10 = <input class=\"test-words__input\" value=\"\">\n" +
+        "                                                16 - 10 = <input data-number='6' class=\"test-words__input\" value=\"\">\n" +
         "                                            </div>\n" +
         "                                            <div class=\"test-words__cell\">\n" +
         "                                                <img src=\"img/bird4.png\" alt=\"\">\n" +
-        "                                                15 - 10 = <input class=\"test-words__input\" value=\"\">\n" +
+        "                                                15 - 10 = <input data-number='5' class=\"test-words__input\" value=\"\">\n" +
         "                                            </div>\n" +
         "                                            <div class=\"test-words__cell\">\n" +
         "                                                <img src=\"img/bird5.png\" alt=\"\">\n" +
-        "                                                14 - 10 - 1 = <input class=\"test-words__input\" value=\"\">\n" +
+        "                                                14 - 10 - 1 = <input data-number='3' class=\"test-words__input\" value=\"\">\n" +
         "                                            </div>\n" +
         "                                            <div class=\"test-words__cell\">\n" +
-        "                                                <img src=\"img/bird5.png\" alt=\"\">\n" +
-        "                                                14 - 10 - 1 = <input class=\"test-words__input\" value=\"\">\n" +
+        "                                                <img src=\"img/bird6.png\" alt=\"\">\n" +
+        "                                                14 - 10 = <input data-number='4' class=\"test-words__input\" value=\"\">\n" +
         "                                            </div>\n" +
         "                                            <div class=\"test-words__cell\">\n" +
         "                                                <img src=\"img/bird7.png\" alt=\"\">\n" +
-        "                                                3 + 7 - 9 = <input class=\"test-words__input\" value=\"\">\n" +
+        "                                                3 + 7 - 9 = <input data-number='1' class=\"test-words__input\" value=\"\">\n" +
         "                                            </div>\n" +
         "                                        </div>\n" +
         "                                        <div class=\"test-words__item tac\">\n" +
         "                                            <div class=\"test-words__cell\">\n" +
-        "                                                <img src=\"img/crossword3.png\" alt=\"\">\n" +
+        "                                                <table id='birdCrosswordTable'>" +
+        "                                                   <tr>" +
+        "                                                       <td>" +
+        "                                                           <span>1</span>" +
+        "                                                       </td>" +
+        "                                                       <td style='border:none !important'></td><td>А</td><td>И</td><td>С</td><td>Т</td>" +
+        "                                                   </tr>" +
+        "                                                   <tr>" +
+        "                                                       <td>" +
+        "                                                           <span>2</span>" +
+        "                                                       </td>" +
+        "                                                       <td>Г</td><td>О</td><td>Л</td><td>У</td><td>Б</td><td>Ь</td>" +
+        "                                                   </tr>" +
+        "                                                   <tr>" +
+        "                                                       <td>" +
+        "                                                           <span>3</span>" +
+        "                                                       </td>" +
+        "                                                       <td>П</td><td>О</td><td>П</td><td>У</td><td>Г</td><td>А</td><td>Й</td>" +
+        "                                                   </tr>" +
+        "                                                   <tr>" +
+        "                                                       <td>" +
+        "                                                           <span>4</span>" +
+        "                                                       </td>" +
+        "                                                       <td>С</td><td>О</td><td>Р</td><td>О</td><td>К</td><td>А</td>" +
+        "                                                   </tr>" +
+        "                                                   <tr>" +
+        "                                                       <td>" +
+        "                                                           <span>5</span>" +
+        "                                                       </td>" +
+        "                                                       <td>П</td><td>А</td><td>В</td><td>Л</td><td>И</td><td>Н</td>" +
+        "                                                   </tr>" +
+        "                                                   <tr>" +
+        "                                                       <td>" +
+        "                                                           <span>6</span>" +
+        "                                                       </td>" +
+        "                                                       <td>С</td><td>Т</td><td>Р</td><td>А</td><td>У</td><td>С</td>" +
+        "                                                   </tr>" +
+        "                                                   <tr>" +
+        "                                                       <td>" +
+        "                                                           <span>7</span>" +
+        "                                                       </td>" +
+        "                                                       <td>П</td><td>И</td><td>Н</td><td>Г</td><td>В</td><td>И</td><td>Н</td>" +
+        "                                                   </tr>" +
+        "                                               </table>" +
         "                                            </div>\n" +
         "                                        </div>\n" +
-
         "                                </div>\n" +
         "                            </div>";
 
     $container.html(html);
+
+    $(".test-words__input").on("change",function(){
+        var $this = $(this);
+        var lenNumber = $this.data("number");
+
+        if($this.val()==lenNumber){
+            $("#birdCrosswordTable tbody").children().each(function(index){
+                if((index+1)==lenNumber) {
+                    $(this).children().css("color", "black");
+                }
+            });
+        }else{
+            $("#birdCrosswordTable tbody").children().each(function(index){
+                if((index+1)==lenNumber) {
+                    $(this).children().css("color", "white");
+                }
+            });
+        }
+    });
 }
+
